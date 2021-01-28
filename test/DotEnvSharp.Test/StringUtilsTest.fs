98d6872cf6eb3ec3d60948a7eb781d3a4590d1d1
Enrico Galassi <enrico.galassi.88@gmail.com>
2021-01-28 16:52:27 +0100

    Add tests to StringUtils

---
+++ test/DotEnvSharp.Test/StringUtilsTest.fs
@@ -0,0 +1,40 @@
+module DotEnvSharp.StringUtils.Test
+
+open System
+open StringUtils
+open NUnit.Framework
+
+[<TestCase(",", "test1,test2,test3", [|"test1"; "test2"; "test3"|])>]
+[<TestCase("#", "test1,test2,test3", [|"test1,test2,test3"|])>]
+[<TestCase(",", "", [|""|])>]
+let ``split should split properly string input`` (separator, input, expected) =
+    let result = split separator input
+    
+    Assert.AreEqual(expected, result)
+
+[<TestCase("  test1", "test1")>]
+[<TestCase("   test2       ", "test2")>]
+[<TestCase("      ", "")>]
+[<TestCase("", "")>]
+let ``trim should trim properly string input`` (input, expected) =
+    let result = trim input
+    
+    Assert.AreEqual(expected, result)
+
+[<TestCase(null, true)>]
+[<TestCase("", true)>]
+[<TestCase("      ", true)>]
+[<TestCase("   a", false)>]
+let ``isNullOrWhiteSpace should return true if the string is null or white space`` (input, expected) =
+    let result = isNullOrWhiteSpace input
+    
+    Assert.AreEqual(expected, result)
+
+[<TestCase(null, false)>]
+[<TestCase("", false)>]
+[<TestCase("      ", false)>]
+[<TestCase("   a", true)>]
+let ``isNotNullOrWhiteSpace should return true if the string is neither null nor white space`` (input, expected) =
+    let result = isNotNullOrWhiteSpace input
+    
+    Assert.AreEqual(expected, result)
