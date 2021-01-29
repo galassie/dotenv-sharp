66b4c7635340c552809856d21d42d421e4170e4c
Enrico Galassi <enrico.galassi.88@gmail.com>
2021-01-29 19:15:53 +0100

    Add new test for config when specifying different .env file

--- test/DotEnvSharp.Test/ConfigTest.fs
+++ test/DotEnvSharp.Test/ConfigTest.fs
@@ -16,3 +16,16 @@ let ``config with default options should configure properly the environment vari
     Assert.AreEqual("VAL1", Environment.GetEnvironmentVariable("KEY1"))
     Assert.AreEqual("Val2", Environment.GetEnvironmentVariable("Key2"))
     Assert.AreEqual("value3", Environment.GetEnvironmentVariable("key3"))
+
+[<Test>]
+let ``config with different file path option should search for overloaded env file andconfigure properly the environment variables`` () =
+    let result = config { DefaultOptions with FilePath = ".altenv" }
+
+    let expected = [|("KEY1", "VAL1"); ("Key2", "Val2"); ("key3", "value3")|]    
+    match result with
+        | Ok envVariables -> Assert.AreEqual(expected, envVariables)
+        | Error _ -> Assert.Fail("Result should be Ok!")
+
+    Assert.AreEqual("VAL1", Environment.GetEnvironmentVariable("KEY1"))
+    Assert.AreEqual("Val2", Environment.GetEnvironmentVariable("Key2"))
+    Assert.AreEqual("value3", Environment.GetEnvironmentVariable("key3"))
